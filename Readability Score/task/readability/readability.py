56d162a166e3a7382683a0736c9e8aba7135ba99
elena <ikonnikovaev@gmail.com>
2022-06-13 19:56:41 +0300

    Stage 2: average length of the sentence

--- Readability Score/task/readability/readability.py
+++ Readability Score/task/readability/readability.py
@@ -1,11 +1,26 @@
-# write your code here
-def is_simple(text):
-    if len(text) > 100:
+import re
+
+def avg(a):
+    return sum(a) / len(a)
+
+def sentence_len(sentence):
+    words = [w for w in re.split(r'\s', sentence) if w]
+    return len(words)
+
+def is_hard(text):
+    threshold = 10
+    punct = r'[.?!]'
+    sentences = [s for s in re.split(punct, text) if s]
+    # print(sentences)
+    avg_sentence_len = avg([sentence_len(s) for s in sentences])
+    # print(avg_sentence_len)
+    if avg_sentence_len > threshold:
+        return True
+    else:
         return False
-    return True
 
 text = input()
-if is_simple(text):
-    print('EASY')
-else:
+if is_hard(text):
     print('HARD')
+else:
+    print('EASY')
