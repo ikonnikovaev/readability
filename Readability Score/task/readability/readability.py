67ddc40735b3d525ffa67629f609cb06ca221bc3
elena <ikonnikovaev@gmail.com>
2022-06-20 14:34:05 +0300

    Stage 5: more scores

--- Readability Score/task/readability/readability.py
+++ Readability Score/task/readability/readability.py
@@ -2,156 +2,198 @@ import re
 import math
 import argparse
 
-def avg(a):
-    return sum(a) / len(a)
+import argparse
+import math
+import re
 
-def text_to_sentences(text):
-    punct = r'[.?!]'
-    sentences = [s for s in re.split(punct, text) if s]
-    return sentences
+class TextInfo:
+    def __init__(self, text, simple_words):
+        self.text = text
+        self.sentences = self.to_sentences()
+        self.words = self.to_words()
+        self.n_sentences = len(self.sentences)
+        self.n_words = len(self.words)
+        self.n_chars = self.count_chars()
+        self.n_syllables = self.count_syllables()
+        self.n_polysyllables = self.count_polysyllables()
+        self.n_difficult = self.count_difficult(simple_words)
 
-def sentences_in_text(text):
-    sentences = text_to_sentences(text)
-    return len(sentences)
-
-def chars_in_text(text):
-    spaces = r'[ \n\t]'
-    n_chars = 0
-    for ch in text:
-        if re.match(spaces, ch):
-            pass
-        else:
-            n_chars += 1
-    return n_chars
-
-def sentence_to_words(sentence):
-    words = [w for w in re.split(r'\s', sentence) if w]
-    return words
-
-def words_in_sentence(sentence):
-    words = sentence_to_words(sentence)
-    return len(words)
-
-def words_in_text(text):
-    sentences = text_to_sentences(text)
-    n_words = sum([words_in_sentence(s) for s in sentences])
-    return n_words
-
-def syllables_in_word(word):
-    vowels = 'aeiouy'
-    n_syllables = 0
-    for i in range(len(word)):
-        if word[i] in vowels:
-            if i > 0 and word[i - 1] in vowels:
-                pass
-            elif i == len(word) - 1 and word[i] == 'e':
+    def count_chars(self):
+        spaces = r'[ \n\t]'
+        n_chars = 0
+        for ch in self.text:
+            if re.match(spaces, ch):
                 pass
             else:
-                n_syllables += 1
-    return max(n_syllables, 1)
+                n_chars += 1
+        return n_chars
 
-def syllables_in_text(text):
-    n_syllables = 0
-    sentences = text_to_sentences(text)
-    for s in sentences:
-        words = sentence_to_words(s)
-        for w in words:
-            n_syllables += syllables_in_word(w)
-    return n_syllables
+    def to_sentences(self):
+        punct = r'[.?!]'
+        sentences = [s for s in re.split(punct, text) if s]
+        return sentences
 
-def polysyllables_in_text(text):
-    n_polysyllables = 0
-    sentences = text_to_sentences(text)
-    for s in sentences:
-        words = sentence_to_words(s)
-        for w in words:
-            k = syllables_in_word(w)
+    def to_words(self):
+        words = []
+        for sentence in self.sentences:
+            words.extend([w for w in re.split(r'[.!?(),\s]+', sentence) if w])
+        return words
+
+    def count_syllables_in_word(self, word):
+        vowels = 'aeiouy'
+        n_syllables = 0
+        for i in range(len(word)):
+            if word[i] in vowels:
+                if i > 0 and word[i - 1] in vowels:
+                    pass
+                elif i == len(word) - 1 and word[i] == 'e':
+                    pass
+                else:
+                    n_syllables += 1
+        return max(n_syllables, 1)
+
+    def count_syllables(self):
+        return sum([self.count_syllables_in_word(w) for w in self.words])
+
+    def count_polysyllables(self):
+        n_polysyllables = 0
+        for w in self.words:
+            k = self.count_syllables_in_word(w)
             if k > 2:
                 n_polysyllables += 1
-    return n_polysyllables
+        return n_polysyllables
 
-def is_hard(text):
-    threshold = 10
-    punct = r'[.?!]'
-    sentences = [s for s in re.split(punct, text) if s]
-    # print(sentences)
-    avg_sentence_len = avg([words_in_sentence(s) for s in sentences])
-    # print(avg_sentence_len)
-    if avg_sentence_len > threshold:
-        return True
-    else:
-        return False
+    def count_difficult(self, simple_words):
+        n_difficult = 0
+        for word in self.words:
+            if not word.lower() in simple_words:
+                n_difficult += 1
+        return n_difficult
 
-def age_by_score(score):
-    # dict = {1: '5-6', 2: '6-7', 3: '7-9', 4: '9-10', 5: '10-11',
-            #6: '11-12', 7: '12-13', 8: '13-14', 9: '14-15', 10: '15-16',
-            #11: '16-17', 12: '17-18', 13: '18-24', 14: '24+'}
-    if score < 3:
-        return score + 5
-    elif score < 13:
-        return score + 6
-    elif score == 13:
+    def print_info(self):
+        print(f'Words: {self.n_words}')
+        print(f'Difficult words: {self.n_difficult}')
+        print(f'Sentences: {self.n_sentences}')
+        print(f'Characters: {self.n_chars}')
+        print(f'Syllables: {self.n_syllables}')
+        print(f'Polysyllables: {self.n_polysyllables}')
+
+
+def avg(a):
+    if not a:
+        return 0
+    return sum(a) / len(a)
+
+class ReadabilityScorer:
+    def __init__(self, text_info):
+        self.text_info = text_info
+
+    def calc_ari(self):
+        score = 4.71 * self.text_info.n_chars / self.text_info.n_words
+        score += 0.5 * self.text_info.n_words / self.text_info.n_sentences
+        score -= 21.43
+        return math.ceil(score)
+
+    def calc_fk(self):
+        score = 0.39 * self.text_info.n_words / self.text_info.n_sentences
+        score += 11.8 * self.text_info.n_syllables / self.text_info.n_words
+        score -= 15.59
+        return math.ceil(score)
+
+    def calc_smog(self):
+        score = 1.043 * math.sqrt(self.text_info.n_polysyllables * 30 / self.text_info.n_sentences) + 3.1291
+        return math.ceil(score)
+
+    def calc_cl(self):
+        l = 100 * self.text_info.n_chars / self.text_info.n_words
+        s = 100 * self.text_info.n_sentences / self.text_info.n_words
+        score = 0.0588 * l - 0.296 * s - 15.8
+        return math.ceil(score)
+
+    def calc_dc(self):
+        p = self.text_info.n_difficult / self.text_info.n_words
+        score = 0.1579 * p * 100 + 0.0496 * self.text_info.n_words / self.text_info.n_sentences
+        if p < 0.05:
+            pass
+        else:
+            score += 3.6365
+        return round(score, 2)
+
+    def score_to_age(self, score):
+        if score < 3:
+            return score + 5
+        elif score < 13:
+            return score + 6
+        elif score == 13:
+            return 24
+        return 25
+
+    def score_to_age_for_dc(self, score):
+        if score < 5.0:
+            return 10
+        if score < 6.0:
+            return 12
+        if score < 7.0:
+            return 14
+        if score < 8.0:
+            return 16
+        if score < 9.0:
+            return 18
         return 24
-    return 25
-
-
-
-def calc_ari(n_chars, n_words, n_sentences):
-    score = 4.71 * n_chars / n_words + 0.5 * n_words / n_sentences - 21.43
-    return math.ceil(score)
-
-def calc_fk(n_syllables, n_words, n_sentences):
-    score = 0.39 * n_words/ n_sentences + 11.8 * n_syllables / n_words - 15.59
-    return math.ceil(score)
-
-def calc_cl(n_chars, n_words, n_sentences):
-    l = 100 * n_chars / n_words
-    s = 100 * n_sentences / n_words
-    score = 0.0588 * l - 0.296 * s - 15.8
-    return math.ceil(score)
 
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--infile')
+parser.add_argument('--words')
 args = parser.parse_args()
-infile = open(args.infile)
-text = infile.read()
-infile.close()
 
+words_file = open(args.words)
+simple_words = words_file.read().split()
+words_file.close()
 
-n_chars = chars_in_text(text)
-n_sentences = sentences_in_text(text)
-n_words = words_in_text(text)
-n_syllables = syllables_in_text(text)
-n_polysyllables = polysyllables_in_text(text)
-print(f'Words: {n_words}')
-print(f'Sentences: {n_sentences}')
-print(f'Characters: {n_chars}')
-print(f'Syllables: {n_syllables}')
-print(f'Polysyllables: {n_polysyllables}')
+text_file = open(args.infile)
+text = text_file.read()
+text_file.close()
 
-print('Enter the score you want to calculate (ARI, FK, CL, all):')
+text_info = TextInfo(text, simple_words)
+text_info.print_info()
+
+scorer = ReadabilityScorer(text_info)
+
+print('Enter the score you want to calculate (ARI, FK, SMOG, CL, DC, all):')
 indices = input().split()
 if 'all' in indices:
-    indices = ['ARI', 'FK', 'CL']
+    indices = ['ARI', 'FK', 'SMOG', 'CL', 'DC']
 ages = []
 for ind in indices:
+    ind = ind.upper()
     if ind == 'ARI':
-        ari = calc_ari(n_chars, n_words, n_sentences)
-        age = age_by_score(ari)
+        ari = scorer.calc_ari()
+        age = scorer.score_to_age(ari)
         ages.append(age)
         print(f'Automated Readability Index: {ari} (about {age} year olds).')
+    elif ind == 'SMOG':
+        smog = scorer.calc_smog()
+        age = scorer.score_to_age(smog)
+        ages.append(age)
+        print(f'Simple Measure of Gobbledygook: {smog} (about {age} year olds).')
     elif ind == 'FK':
-        fk = calc_fk(n_syllables, n_words, n_sentences)
-        age = age_by_score(fk)
+        fk = scorer.calc_fk()
+        age = scorer.score_to_age(fk)
         ages.append(age)
         print(f'Flesch–Kincaid readability tests: {fk} (about {age} year olds).')
     elif ind == 'CL':
-        cl = calc_cl(n_chars, n_words, n_sentences)
-        age = age_by_score(cl)
+        cl = scorer.calc_cl()
+        age = scorer.score_to_age(cl)
         ages.append(age)
         print(f'Coleman–Liau index: {cl} (about {age} year olds).')
+    elif ind == 'DC':
+        dc = scorer.calc_dc()
+        age = scorer.score_to_age_for_dc(dc)
+        ages.append(age)
+        print(f'Dale-Chall score: {dc} (about {age} year olds).')
+
 avg_age = avg(ages)
 print(f'This text should be understood in average by {round(avg_age, 0)} year olds.')
 
